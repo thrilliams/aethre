981407378aa92708d0e97eed1088fb03390593d9
june <juniper@aethre.co>
2018-12-12 08:04:45 -0800

    ?

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// packages
 var express = require('express')
+var subdomain = require('express-subdomain')
+
+// subdomains
+var crosscode = express.Router()
+
+// app
 var app = express()
+
+
+// routing
+crosscode.use(express.static('dist/crosscode'))
+
+app.use(subdomain('crosscode', crosscode))
 
 app.get('/', (req, res) => {
   var sub = req.subdomains.reverse().join('.')
@@ -7,4 +20,5 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/index.html')
 })
 
+// listen
 app.listen(process.env.PORT || 8080)
