9827a29ebe8f0b1e90e5f33c8f918fb4e9a78ac4
thrilliams <sorenw@khanlabschool.org>
2018-12-11 19:19:51 -0800

    ops

--- crosscode/impact/page/php/game.beta.php
+++
@@ -1,4 +0,0 @@
-<script>
-  var IG_GAME_BETA = true;
-</script>
-<script type="text/javascript" src="<?= $IG_ROOT ?>js/game.compiled.js<?= $IG_GAME_CACHE ?>"></script>
--- crosscode/impact/page/php/game.debug.php
+++
@@ -1,6 +0,0 @@
-<script>
-  var IG_GAME_DEBUG = true;
-  var IG_GAME_BETA = true;
-</script>
-<script type="text/javascript" src="<?= $IG_ROOT ?>impact/base/impact.js<?= $IG_GAME_CACHE ?>"></script>
-<script type="text/javascript" src="<?= $IG_ROOT ?>game/main.js<?= $IG_GAME_CACHE ?>"></script>
