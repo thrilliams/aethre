82a1a97714c25b0cbcaefb155d921ea80ce48c48
june <juniper@aethre.co>
2019-05-03 15:30:01 -0700

    add close button to increase userability

--- static/index.js
+++ static/index.js
@@ -13,7 +13,12 @@ $.ajax({
 					article = $('<div><h1>' + $(page)[0].innerText + '</h1></div>')
 					$('#content').append(article)
 					article.click(e => {
-						$('#article').html(`<div>${page}</div>`)
+						$('#article').html(`<div><span class="x"></span>${page}</div>`)
+						$('.x').click(e => {
+							$('#article').html('')
+							$('#content').removeClass('hidden')
+							$('#article').addClass('hidden')
+						})
 						$('#article').removeClass('hidden')
 						$('#content').addClass('hidden')
 					})
@@ -21,10 +26,4 @@ $.ajax({
 			})
 		}
 	}
-})
-
-$('#article').click(e => {
-	$('#article').html('')
-	$('#content').removeClass('hidden')
-	$('#article').addClass('hidden')
 })
