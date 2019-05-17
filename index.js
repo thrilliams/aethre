40d3914a4249128af040460d80a04f9081c2f957
thrilliams <sorenw@khanlabschool.org>
2019-05-17 15:44:08 -0700

    add krc

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var subdomain = require('express-subdomain')
 var express = require('express')
 var marked = require('marked')
 var fs = require('fs')
 var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+
+app.use(subdomain('within', require('./within/index.js')(io)))
 
 app.use('/static', express.static('static'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
@@ -21,5 +26,5 @@ app.get('/pages/*', (req, res) => {
     res.end(marked(fs.readFileSync(req.url.slice(1), 'utf8')))
 })
 
+server.listen(process.env.PORT || 8080)
 console.log(`Listening on port ${process.env.PORT || 8080}`)
-app.listen(process.env.PORT || 8080)
