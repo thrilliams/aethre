aff20d599e35e7aa556aa409c7391b95b7e18d2e
Soren <soren@aethre.co>
2021-08-11 09:09:30 -0700

    update marked

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-var subdomain = require('express-subdomain')
-var express = require('express')
-var marked = require('marked')
-var fs = require('fs')
-var app = express()
-var io = require('socket.io')(app.listen(process.env.PORT || 8080))
+const subdomain = require('express-subdomain');
+const express = require('express');
+const marked = require('marked');
+const fs = require('fs');
+const app = express();
+const io = require('socket.io')(app.listen(process.env.PORT || 8080));
 
-app.use(subdomain('krc', require('./within/index.js')(io)))
+app.use(subdomain('krc', require('./within/index.js')(io)));
 
-app.use('/static', express.static('static'))
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+app.use('/static', express.static('static'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.get('/pages', (req, res) => {
-	fs.readdir('pages', (err, items) => {
-		if (err) {
-            res.status(500).end(err)
-            return console.log(err)
+    fs.readdir('pages', (err, items) => {
+        if (err) {
+            res.status(500).end(err);
+            return console.log(err);
         }
 
-        res.json(items)
-	})
-})
+        res.json(items);
+    });
+});
 
 app.get('/pages/*', (req, res) => {
-    res.end(marked(fs.readFileSync(req.url.slice(1), 'utf8')))
-})
+    res.end(marked(fs.readFileSync(req.url.slice(1), 'utf8')));
+});
 
-console.log(`Listening on port ${process.env.PORT || 8080}`)
+console.log(`Listening on port ${process.env.PORT || 8080}`);
